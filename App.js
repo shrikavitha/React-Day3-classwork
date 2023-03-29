9e2b5a92224136e32040aa994a425aff74c7f240
shrikavitha <108916475+shrikavitha@users.noreply.github.com>
2023-03-29 12:00:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Daythree from './Daythree'
+import Naving from './Naving'
+
+function App() {
+  return (
+    <div>
+      <Daythree/>
+      <Naving/>
+    </div>
+  )
+}
+
+export default App
